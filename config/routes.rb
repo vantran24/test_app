e2369fd98e23da907b235ba9deb61a5173a9d1fb
Van <vvtran@wisc.edu>
2013-11-02 15:27:46 -0500

    new home

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ TestApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # edited the root site/home page
-  #root 'users#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
